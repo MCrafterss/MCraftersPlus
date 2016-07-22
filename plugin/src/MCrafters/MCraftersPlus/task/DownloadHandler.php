e137c896a88ffd78cac08ef430bd077169eada94
Survingo <thehydra.tk@gmail.com>
2016-07-22 20:28:41 +0200

    Update DownloadHandler.php

--- plugin/src/MCrafters/MCraftersPlus/task/DownloadHandler.php
+++ plugin/src/MCrafters/MCraftersPlus/task/DownloadHandler.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * __  __  ____            __ _                        
+ *|  \/  |/ ___|_ __ __ _ / _| |_ ___ _ __ ___     _   
+ *| |\/| | |   | '__/ _` | |_| __/ _ \ '__/ __|  _| |_ 
+ *| |  | | |___| | | (_| |  _| ||  __/ |  \__ \ |_   _|
+ *|_|  |_|\____|_|  \__,_|_|  \__\___|_|  |___/   |_|  
+ *
+*/
+
 namespace MCrafters\MCraftersPlus\task;
 
 use pocketmine\scheduler\AsyncTask;
@@ -7,14 +16,15 @@ use MCrafters\MCraftersPlus\Main;
 
 class DownloadHandler extends AsyncTask{
   
-  private $plugin;
-  
-  public function __construct(Main $plugin){
-    $this->plugin = $plugin;
+  public function __construct($url, $folder){
+    $this->url = $url;
+    $this->folder = $folder;
   }
   
   public function onRun(){
-    //download
+    if(is_string(($download = \pocketmine\utils\Utils::getURL($this->url)))){
+      file_put_contents($this->folder, $download);
+    }
   }
   
 }
