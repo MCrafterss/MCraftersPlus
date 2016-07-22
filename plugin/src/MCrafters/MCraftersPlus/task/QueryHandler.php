c982b009586676d1b1e616364119518c15047f38
Survingo <thehydra.tk@gmail.com>
2016-07-22 20:53:08 +0200

    Update QueryHandler.php

--- plugin/src/MCrafters/MCraftersPlus/task/QueryHandler.php
+++ plugin/src/MCrafters/MCraftersPlus/task/QueryHandler.php
@@ -13,24 +13,25 @@ namespace MCrafters\MCraftersPlus\task;
 
 
 use pocketmine\scheduler\PluginTask;
+use MCrafters\MCraftersPlus\Main;
 
 class QueryHandler extends PluginTask{
   
   private $plugin;
   
   public function __construct(Main $plugin, $name){
-    parent::__construct($plugin);
     $this->plugin = $plugin;
+    $this->name = $name;
   }
   
   public function onRun($currentTick){
-    $url = \pocketmine\utils\Utils::getURL("https://github.com/MCrafterss/MCraftersPlus/blob/master/data/plugins/$name.yml");
+    $url = \pocketmine\utils\Utils::getURL("https://github.com/MCrafterss/MCraftersPlus/blob/master/data/plugins/" . $this->name . ".yml");
     $parse = yaml_parse($url);
-    if($parse["version"] !== $this->plugin->getServer()->getPluginManager()->getPlugin($name)->getDescription()->getVersion()){
+    if($parse["version"] !== $this->plugin->getServer()->getPluginManager()->getPlugin($this->name)->getDescription()->getVersion()){
       $this->plugin->getLogger()->info("\n------------------------\n§1M§9Crafters+ §5Auto Updater§f\nA new version of $name has been released! Do you want to update your current installed version " . $this->plugin->getServer()->getPluginManager()->getPlugin($name)->getDescription()->getVersion() . " to the new " . $parse["version"] . "?\n§7Update description:§f\n" . $parse["description"] . "\n(§ayes§f/§cno§f)");
       if(strtolower($this->plugin->getInput("no")) === "yes") continue;
       $this->plugin->getLogger()->info("Updating plugin...");
-      $this->plugin->getServer()->getScheduler()->scheduleAsyncTask(new DownloadHandler($parse["download"], $this->plugin->getDataFolder() . "/" . $filename));
+      $this->plugin->getServer()->getScheduler()->scheduleAsyncTask(new DownloadHandler($parse["download"], $this->plugin->getDataFolder() . "/" . $name . ".phar"));
     }
   }
   
