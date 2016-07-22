091eae9c6007f33f8ac3b6f72656d89828433235
Survingo <thehydra.tk@gmail.com>
2016-07-22 07:52:10 +0200

    Update Main.php

--- plugin/src/MCrafters/MCraftersPlus/Main.php
+++ plugin/src/MCrafters/MCraftersPlus/Main.php
@@ -1,7 +1,31 @@
 <?php
 
+/*
+ * __  __  ____            __ _                        
+ *|  \/  |/ ___|_ __ __ _ / _| |_ ___ _ __ ___     _   
+ *| |\/| | |   | '__/ _` | |_| __/ _ \ '__/ __|  _| |_ 
+ *| |  | | |___| | | (_| |  _| ||  __/ |  \__ \ |_   _|
+ *|_|  |_|\____|_|  \__,_|_|  \__\___|_|  |___/   |_|  
+ *
+*/
+
 namespace MCrafters\MCraftersPlus;
 
 use pocketmine\plugin\PluginBase;
 
-class Main extends PluginBase
+class Main extends PluginBase{
+  
+  public function onEnable(){
+    $this->getLogger()->info("Enabling MCraftersPlus...");
+  }
+  
+  public function onDisable(){
+    $this->getLogger()->info("Disabling MCraftersPlus...");
+  }
+  
+  public function getInput($default = ""){
+		$input = \trim(\fgets(STDIN));
+		return $input === "" ? $default : $input;
+	}
+  
+}
