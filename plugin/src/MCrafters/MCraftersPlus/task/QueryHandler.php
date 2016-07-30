f1da454b7076fbe11b20238fd9118b22babbd710
xBeastMode <xBeastMode@users.noreply.github.com>
2016-07-30 16:21:26 -0700

    Update QueryHandler.php

--- plugin/src/MCrafters/MCraftersPlus/task/QueryHandler.php
+++ plugin/src/MCrafters/MCraftersPlus/task/QueryHandler.php
@@ -41,7 +41,7 @@ class QueryHandler extends PluginTask{
     $parse = yaml_parse($url);
     if($parse["version"] !== ($plugin = $this->plugin->getServer()->getPluginManager()->getPlugin($this->name))->getDescription()->getVersion()){
       $this->plugin->getLogger()->info("\n------------------------\n§1M§9Crafters+ §5Auto Updater§f\nA new version of $name has been released! Do you want to update your current installed version " . $this->plugin->getServer()->getPluginManager()->getPlugin($name)->getDescription()->getVersion() . " to the new " . $parse["version"] . "?\n§7Update description:§f\n" . $parse["description"] . "\n(§ayes§f/§cno§f)");
-      if(strtolower($this->plugin->getInput("no"))) === "yes" ? : $this->plugin->getLogger()->info("Update cancelled!");
+      if(strtolower($this->plugin->getInput("no")) === "yes" ? : $this->plugin->getLogger()->info("Update cancelled!");
       $this->plugin->getLogger()->info("Disabling plugin...");
       $this->plugin->getServer()->getPluginManager()->disablePlugin($plugin);
       $this->plugin->getLogger()->info("Downloading plugin... Please wait...");
