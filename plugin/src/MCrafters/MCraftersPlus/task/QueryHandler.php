e6b226a6b81bb135c6c892f416588695ad2908e3
Survingo <thehydra.tk@gmail.com>
2016-08-16 19:16:16 +0200

    Fixes

--- plugin/src/MCrafters/MCraftersPlus/task/QueryHandler.php
+++ plugin/src/MCrafters/MCraftersPlus/task/QueryHandler.php
@@ -40,25 +40,33 @@ class QueryHandler extends PluginTask{
     $url = \pocketmine\utils\Utils::getURL("https://github.com/MCrafterss/MCraftersPlus/blob/master/data/plugins/" . $this->name . ".yml");
     $parse = yaml_parse($url);
     if($parse["version"] !== ($plugin = $this->plugin->getServer()->getPluginManager()->getPlugin($this->name))->getDescription()->getVersion()){
-      $this->plugin->getLogger()->info("\n------------------------\n§1M§9Crafters+ §5Auto Updater§f\nA new version of $name has been released! Do you want to update your current installed version " . $this->plugin->getServer()->getPluginManager()->getPlugin($name)->getDescription()->getVersion() . " to the new " . $parse["version"] . "?\n§7Update description:§f\n" . $parse["description"] . "\n(§ayes§f/§cno§f)");
-      if(strtolower($this->plugin->getInput("no")) === "yes"){
-        $this->plugin->getLogger()->info("Update cancelled!");
+      $this->plugin->getLogger()->info($this->plugin->getCenteredLine("\n------------------------\n§1M§9Crafters+ §5Auto Updater§f\nA new version of $name has been released! Do you want to update your current installed version " . $this->plugin->getServer()->getPluginManager()->getPlugin($name)->getDescription()->getVersion() . " to the new " . $parse["version"] . "?\n§7Update description:§f\n" . $parse["description"] . "\n(§ayes§f/§cno§f)"));
+      if($this->plugin->getConfig()->get("force_auto_update")){
+        $this->plugin->getLogger()->info("Force Auto Update enabled! Updating now...");
       }else{
-        $this->plugin->getLogger()->info("Update cancelled!");
-        return;//To do: Save statement so it won't spam you every 30 minutes
+        if(strtolower($this->plugin->getInput("no")) === "yes"){
+          // continue
+        }else{
+          $this->plugin->getLogger()->info("Update cancelled!");
+          return;
+        }
       }
       $this->plugin->getLogger()->info("Disabling plugin...");
       $this->plugin->getServer()->getPluginManager()->disablePlugin($plugin);
       $this->plugin->getLogger()->info("Downloading plugin... Please wait...");
       $this->plugin->getServer()->getScheduler()->scheduleAsyncTask(new DownloadHandler($parse["download"], $this->plugin->getDataFolder() . "/" . $this->name . ".phar"));
+      sleep($this->plugin->getConfig()->get("download_time"));
       if(file_exists($this->plugin->getDataFolder() . "/" . $this->name . ".phar")){
         $this->plugin->getLogger()->info($this->name . " downloaded. Restarting plugin now...");
+        $this->plugin->getServer()->forceShutdown();
+        sleep(1);
         $reflection = new \ReflectionClass("pocketmine\\plugin\\PluginBase");
         $file = $reflection->getProperty("file");
         $file->setAccessible(true);
         $this->plugin->force_delete($file->getValue($plugin));
         copy($this->plugin->getDataFolder() . "/" . $this->name . ".phar", $this->plugin->getServer()->getDataFolder() . "/plugins/" . $this->name . ".phar");
         $this->plugin->force_delete($this->plugin->getDataFolder() . "/" . $this->name . ".phar");
+        shell_exec($this->plugin->getConfig()->get("start_exec"));
       }else{
         $this->plugin->getLogger()->warning("Something went wrong! The update couldn't be installed!");
         return;
